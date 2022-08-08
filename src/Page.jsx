de08e4f1c8f209c34d07d10c319473a5c0a991be
Lena <z.otig@yahoo.com>
2022-08-08 04:08:35 -0600

    open image

--- src/Page.jsx
+++ src/Page.jsx
@@ -8,14 +8,12 @@ const Page = () => {
   let container = useRef(null);
   let image = useRef(null);
 
-  useEffect(() => {
-    console.log(container);
-    console.log(image);
-  }, []);
+  useEffect(() => {}, []);
   return (
     <section>
       <div className="page">
         <>
+          <h1>Video</h1>
           <motion.div
             ref={(el) => (container = el)}
             className="image-container"
@@ -26,12 +24,18 @@ const Page = () => {
               transition={{
                 easing: [0.42, 0, 0.58, 1],
                 delay: 1,
-                duration: 0.5,
+                duration: 0.6,
               }}
               ref={(el) => (image = el)}
               src={img}
               alt="promo"
             ></motion.img>
+            <motion.div
+              initial={{ width: `100%` }}
+              animate={{ width: 0 }}
+              transition={{ delay: 1, duration: 0.7 }}
+              className="animated-block"
+            ></motion.div>
           </motion.div>
         </>
       </div>
